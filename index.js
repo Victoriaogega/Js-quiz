20bc63f668492240fd76649a8bd2446f7b66b8ac
Victoria-Ogega <victoriaogega@gmail.com>
2025-06-02 08:03:30 +0300

    re-did the work

--- index.js
+++ index.js
@@ -15,31 +15,46 @@ class CustomerOrder{
     calculateTotal() {
    return this.items.reduce((acc, item)=> acc + (item.quantity * item.price),0)
         }
+
+    async processPayment(){
+      await new Promise((resolve)=>{
+        setTimeout(()=>{
+         this.status = "paid"
+          resolve("Payment successful")
+          console.log(`Your payment of order ${this.orderId} has been successful and the payment status is ${this.status}.`);
+        },2000);
+      })
+    } 
 }
-async function processPayment() {
-  await new Promise((resolve,reject)=> setTimeout(resolve,2000))
-   if(this.status = "paid"){resolve(`Your payment of ${this.orderId} has been successful.`)}
-}
-
-let customer = new CustomerOrder("cu11",[
-    {name:"waterbottle",quantity:2,price:1000},
-    {name:"headphones",quantity:1,price:2500},
-    {name:"stickynotes",quantity:2,price:250}
-])
 
 
-console.log(customer.processPayment);
- console.log(customer.calculateTotal());
+  let order = new CustomerOrder("OD001",
+    [ {name:"Laptop",quantity:10,price:150000},
+     {name:"phone",quantity:15,price:55000},
+      {name:"headphones",quantity:5,price:10000}],
+  "pending")
+ 
+ 
+ console.log(order.processPayment());
+ 
+  console.log(order.calculateTotal());
+   
+ 
+ 
+  
+  
   
 
 
 
-// Question 2
-// Create a TeamMember class that takes name, role, and an array of tasks (each task is an object with title and completed boolean). Write a prototype method completeTask(taskTitle) that marks a task as completed. Write another method checkProgress() that returns a Promise resolving to "All tasks completed!" or rejecting with "Pending tasks remaining" based on the state of the tasks array.
-// pseudocodes
-// check if the completion of a tsk is true
-// if it is true, mark it as completed
-// checkProgress by looking at all the completion status and use promises to resolve or reject.
+// // Question 2
+// // Create a TeamMember class that takes name, role, and an array of tasks (each task is an object with title and completed boolean). Write a prototype method completeTask(taskTitle) that marks a task as completed. Write another method checkProgress() that returns a Promise resolving to "All tasks completed!" or rejecting with "Pending tasks remaining" based on the state of the tasks array.
+
+// // pseudocodes
+// // check if the task title exists in the array of objects which has the titles and the completion status of each task.
+// // if ot is there, check the status of the tas
+// // if it is true, mark it as completed
+// // checkProgress by looking at all the completion status and use promises to resolve if it is completed and reject if all tasks are not completed.
 
 
 class TeamMember{
@@ -50,24 +65,36 @@ class TeamMember{
     }
 }
 TeamMember.prototype.completeTask = function(taskTitle){
-let completionStatus=  this.tasks.filter(task => task.completion == true) 
-completionStatus = "Completed"
-return completionStatus
+let completedTasks = this.tasks.find(task => task.title === taskTitle)
+if(completedTasks){
+  completedTasks.completed = true;
+  console.log(`Task ${taskTitle} for ${this.name} has been completed.`); 
+}else{
+  console.log(`Task ${taskTitle} is not availavle for ${this.name}`);
+  
+}
 }
 
-TeamMember.prototype.checkProgress = new Promise((resolve,reject)=>{
-    resolve("All tasks completed")
-    reject(`${this.tasks.completion == true} tasks remaining`)
-})
+TeamMember.prototype.checkProgress = function(){
+  return new Promise((resolve,reject)=>{
+    let allCompletedTasks = this.tasks.every(task=>{ task.completed});
+    if(allCompletedTasks){
+      resolve("All tasks completed")
+    }else{reject("Pending tasks remaining")}
+      return allCompletedTasks
+  })
+
+}
 
 let victoria = new TeamMember("victoria","student",[
     {title:"DAS", completion:false},
     {title:"js",completion: true}
 ])
 
-console.log(victoria.completeTask());
-console.log(victoria.checkProgress);
-
+victoria.completeTask("js")
+victoria.checkProgress()
+.then(result=> console.log(result))
+.catch(error => console.log(error))
 
 
 
@@ -79,34 +106,38 @@ class Candidate{
         this.interviews = interviews
     }
     scheduleInterviews(date){
-        this.interviews.push({date:"12-07-2025",status:"pending"})
+        this.interviews.push({date,status:"pending"})
         return this.interviews
     }
-}
-async function sendConfirmation(){
-await new Promise((resolve,reject)=> setTimeout(resolve,1000))
+
+async sendConfirmation(){
+return  new Promise((resolve)=>{
+  setTimeout(()=>{
 if(this.interviews.status === "confirmed"){
+}
 resolve("Interview confirmed")
 console.log(`${this.name} your interview has been confirmed.`);
+  },1000)
+})
 }
-
 }
-
 let candidate = new Candidate("Ogega","machine learning engineer",[
     {date:"12-24-2025",status:"pending"},
     {date:"02-07-2025",status:"pending"},
     {date:"23-07-2025",status:"confirmed"}
 ])
-candidate.scheduleInterviews()
-candidate.sendConfirmation()
+
+console.log(candidate.scheduleInterviews("12-07-2025"));
+console.log(candidate.sendConfirmation());
 
 
 
-// Design a Course class with properties: title, instructor, and students (array of student objects with name and progress). Add a method updateProgress(studentName, value) that modifies the student’s progress. Create an async method generateCertificate(studentName) that returns a Promise resolving only if the progress is 100, otherwise reject with "Incomplete progress".
-// pseudocode
-// create a class with its properties
-// create a method update Progress that modifies the students progress by replacing the old value with the new value
-// Create a method generate certificate that only gives certificates to students whose progress is 100 only. 
+
+// // Design a Course class with properties: title, instructor, and students (array of student objects with name and progress). Add a method updateProgress(studentName, value) that modifies the student’s progress. Create an async method generateCertificate(studentName) that returns a Promise resolving only if the progress is 100, otherwise reject with "Incomplete progress".
+// // pseudocode
+// // create a class with its properties
+// // create a method update Progress that modifies the students progress by replacing the old value with the new value
+// // Create a method generate certificate that only gives certificates to students whose progress is 100 only. 
 
 
 
@@ -116,21 +147,22 @@ this.title = title
 this.instructor = instructor
 this.students = students
     }
-    updateProgress(studentName,value){
-let updatedValues = {name:studentName,progress:value}
-Object.assign(this.students,updatedValues)
+  updateProgress(studentName,value){
+let studentProgress = this.students.find(student=> student.name === studentName)
+if(studentProgress) studentProgress.progress = value
 return this.students
-    }
 }
-async function generateCertificate(studentName) {
-   new Promise ((resolve,reject)) 
-   try{if(this.students.progress >= 100){
-    resolve(`${studentName} you have received your certificate, congratulatuos!!` )
-   } }catch(error){
-    console.log("Incomplete progress.");
-    
-   } 
+
+async  generateCertificate(studentName) {
+  return new Promise ((resolve,reject)=>{
+    if(this.students.progress < 100){
+   reject ("Incomplete progress")
+   } else{ resolve(`${studentName} you have received your certificate, congratulatulations!!` ) }
+  }) 
+   
 }
+}
+
 
 let course = new Course("Mathematics","Ochieng",[
     {name:"tesfay",progress:60},
@@ -138,19 +170,48 @@ let course = new Course("Mathematics","Ochieng",[
     {name:"mahado",progress:100}
 
 ])
-course.updateProgress()
-course.generateCertificate()
+
+console.log(course.updateProgress("tesfay",79));
+course.generateCertificate("mahado")
+.then(result =>console.log(result))
+.catch(error => console.log(error));
 
 
-// Create a StockTracker class with a property watchlist (array of objects with symbol, threshold, currentPrice). Add a method updatePrice(symbol, newPrice) that updates the stock’s current price. Write an async method checkAlerts() that loops through the watchlist and returns a Promise resolving with a list of stocks where currentPrice >= threshold, or rejecting with "No alerts triggered".
+
+
+
+// // Create a StockTracker class with a property watchlist (array of objects with symbol, threshold, currentPrice). Add a method updatePrice(symbol, newPrice) that updates the stock’s current price. Write an async method checkAlerts() that loops through the watchlist and returns a Promise resolving with a list of stocks where currentPrice >= threshold, or rejecting with "No alerts triggered".
+
 class StockTracker{
-    constructor(watchlist,){
+constructor(watchlist){
 this.watchlist = watchlist
     }
+
 updatePrice(symbol, newPrice){
-    
+ let updatedPrice = this.watchlist.find(price => price.symbol === symbol)
+ if(updatedPrice) updatedPrice.currentPrice = value
+ return this.watchlist  
 }
 }
 async function checkAlerts() {
-    
+    return new Promise((resolve,reject)=>{
+    const alerts = this.watchlist.filter( stock => stock.currentPrice >= stock.threshold)
+    if(alerts.lenth > 0){resolve (alerts)}
+    else{reject("No alerts triggered")}
+    })  
+     
 }
+
+
+let tracker = new StockTracker([{symbol:"aaa",threshold:"150, price:1500"},
+    {symbol:"aaa",threshold:"150, price:1500"},
+    {symbol:"ccc",threshold:"120, price:2500"},
+    {symbol:"bbb",threshold:"100, price:500"}
+])
+
+
+console.log( tracker.updatePrice());
+console.log(tracker.checkAlerts);
+
+
+
